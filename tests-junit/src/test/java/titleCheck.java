5de931b286ad3b8d26092100958b084bbe8a17e6
y.sveshnikov <ya.sveshnikov@space307.com>
2020-07-07 00:25:44 +0300

    add-homework-1

--- tests-junit/src/test/java/titleCheck.java
+++ tests-junit/src/test/java/titleCheck.java
@@ -1,3 +1,4 @@
+import com.sun.org.apache.xpath.internal.operations.Equals;
 import config.ServerConfig;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.aeonbits.owner.ConfigFactory;
@@ -6,13 +7,17 @@ import org.apache.logging.log4j.Logger;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class SampleTest {
+import static org.junit.Assert.assertEquals;
+
+public class titleCheck {
 
     protected static WebDriver driver;
-    private Logger logger = LogManager.getLogger(SampleTest.class);
+    private Logger logger = LogManager.getLogger(titleCheck.class);
     private ServerConfig cfg = ConfigFactory.create(ServerConfig.class);
 
     @Before
@@ -20,12 +25,15 @@ public class SampleTest {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         logger.info("Драйвер поднят");
+        driver.get(cfg.url());
+        logger.info("Открыта страница отус");
     }
 
     @Test
-    public void openPage() {
-        driver.get(cfg.url());
-        logger.info("Открыта страница отус");
+    public void titleEquals() {
+        String title = "Онлайн‑курсы для профессионалов, дистанционное обучение современным профессиям";
+        assertEquals(title, driver.getTitle());
+        logger.info("Проверка title");
     }
 
     @After
